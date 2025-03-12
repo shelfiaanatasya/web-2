fd1af493e9cdc65e90b79fd583c16a1acebdd117
Shlfiantsya <0110124019@student.nurulfikri.ac.id>
2025-03-12 19:20:19 +0700

    upload file

--- praktikum-3b/index.php
+++ praktikum-3b/index.php
@@ -1,15 +1,12 @@
 <?php
-require_once("function/CallPage.php");
+require_once("function/callpage.php");
+
 callPage("navbar");
 callPage("header");
 if (isset($_GET['page'])) {
     callPage($_GET['page']);
-} else{
-    callPage("home");
-   
+} else {
+    callPage("home"); 
 }
 callPage("footer");
-callPage("about")
-
-
 ?>
